f055970d452ea0c2cd1a142a484e3959024220a4
Damian Adams <contact@mianlabs.com>
2022-07-13 08:45:26 -0500

    Format files with Prettier

--- src/composables/use8baseStorage.ts
+++ src/composables/use8baseStorage.ts
@@ -1,5 +1,5 @@
-import gql from "graphql-tag";
-import { useMutation, useQuery } from "@vue/apollo-composable";
+import gql from 'graphql-tag'
+import { useMutation, useQuery } from '@vue/apollo-composable'
 
 const IMAGE_UPLOAD_QUERY = gql`
   query {
@@ -10,7 +10,7 @@ const IMAGE_UPLOAD_QUERY = gql`
       path
     }
   }
-`;
+`
 
 const FILE_CREATE_MUTATION = gql`
   mutation CREATE_FILE($fileId: String!, $filename: String!) {
@@ -18,34 +18,34 @@ const FILE_CREATE_MUTATION = gql`
       id
     }
   }
-`;
+`
 
 export function useStorage() {
-  const { result } = useQuery(IMAGE_UPLOAD_QUERY);
-  const { mutate: createFileIn8base } = useMutation(FILE_CREATE_MUTATION);
+  const { result } = useQuery(IMAGE_UPLOAD_QUERY)
+  const { mutate: createFileIn8base } = useMutation(FILE_CREATE_MUTATION)
 
   async function uploadAsset(file: File) {
     if (!result.value || !result.value.fileUploadInfo) {
-      throw new Error("File Upload info not yet available");
+      throw new Error('File Upload info not yet available')
     }
     const res = await fetch(
       `https://www.filestackapi.com/api/store/S3?key=${result.value.fileUploadInfo.apiKey}&policy=${result.value.fileUploadInfo.policy}&signature=${result.value.fileUploadInfo.signature}&path=${result.value.fileUploadInfo.path}`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": file.type,
+          'Content-Type': file.type,
         },
         body: file,
       }
-    );
-    const data = await res.json();
+    )
+    const data = await res.json()
     return createFileIn8base({
-      fileId: data.url.split("/").at(-1),
+      fileId: data.url.split('/').at(-1),
       filename: data.filename,
-    });
+    })
   }
   return {
     uploadAsset,
-  };
+  }
 }
-export default useStorage;
+export default useStorage
